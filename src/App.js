93abb579e4c0a878ccb394fe31e3f4afc8747956
s-v7 <svasconceloscruz7@gmail.com>
2025-02-04 20:46:59 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,39 +1,19 @@
-//import logo from './logo.svg';
 import './App.css';
-
-/*function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App; */
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
+import Footer from "./components/Footer";
 import About from "./pages/About";
-import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
-import "./styles/global.css";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Skills from "./pages/Skills";
 
-function App() {
+import "./styles/global.css"; // Link simbólico para o arquivo fora de src
+
+
+
+const App = () => {
   return (
     <Router>
       <Navbar />
@@ -42,10 +22,15 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/skills" element={<Skills />} />
       </Routes>
+     <Footer />	  
     </Router>
   );
-}
+};
 
 export default App;
+
+
+
 
