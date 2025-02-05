5eb0a73ab461fb98d1b30fa8e28ef3c99c3df57c
Silas Vasconcelos Cruz <svasconceloscruz7@gmail.com>
2025-02-05 20:42:22 -0300

    feat: update

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -14,9 +14,6 @@ const Projects = () => {
       tecnologias: "Blockchain, Node.js",
       link: "https://github.com/s-v7/blockchain-crea-pi",
     },
-  ];
-
-  const projetos1 = [
     {
       nome: "CadastroRastreamentoObras",
       descricao:
@@ -32,26 +29,11 @@ const Projects = () => {
     },
   ];
 
-  const projetos2 = [
-    {
-      nome: "Task-Managements",
-      descricao: "Aplicativo para auditoria utilizando Flask e Angular.",
-      tecnologias: "Python, SQLite, Neural Networks",
-      link: "https://github.com/s-v7/Task-Managements",
-    },
-    {
-      nome: "Blockchain CREA-PI",
-      descricao: "Rede Blockchain para integração de dados CREA-PI.",
-      tecnologias: "Blockchain, Node.js",
-      link: "https://github.com/s-v7/blockchain-crea-pi",
-    },
-  ];
-
   return (
     <section id="projetos" className="p-6 bg-coffee text-white">
       <h2 className="text-3xl font-bold mb-6 text-center">Projetos</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {[...projetos, ...projetos1, ...projetos2].map((projeto, index) => (
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        {projetos.map((projeto, index) => (
           <div
             key={index}
             className="projeto-card p-4 bg-gray-800 rounded shadow-md hover:shadow-lg transition-shadow"
