3f3bf5f7a696cb344c18719a11ffad367a952de2
s-v7 <svasconceloscruz7@gmail.com>
2025-02-12 10:24:33 -0300

    update

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,15 +1,18 @@
 import React from "react";
+import "./Skills.css"; // Certifique-se de importar os estilos corretamente
 
 const Skills = () => {
   return (
-    <div className="p-6">
-      <h2 className="text-2xl font-bold mb-4">Habilidades</h2>
-      <ul className="list-disc pl-6">
-        <li>Java (JDK 8, JDK 11, JDK 17)</li>
-        <li>React, Angular</li>
-        <li>PostgreSQL, MongoDB</li>
-        <li>Inteligência Artificial</li>
-        <li>DevOps (Docker, Kubernetes)</li>
+    <div className="skills-container">
+      <h1 className="skills-title">Habilidades</h1>
+      <ul className="skills-list">
+        <li>✔️ Backend: Java(JDK(s) 8,11,17),(EJB, JPA, JSF, PrimeFaces), Python(Flask, Django), Node.js</li>
+        <li>✔️ Frontend: React, Angular, JavaScript</li>
+        <li>✔️ Banco de Dados: PostgreSQL, MySQL, SQLite, MongoDB</li>
+        <li>✔️ DevOps e Automação: Gitea, CI/CD, Webhooks, Docker, Kubernetes, Terraform, Ansible</li>
+        <li>✔️ Segurança: OWASP, Hardening de APIs, Pentest, Blockchain, Auditoria e Logs</li>
+        <li>✔️ IA e Machine Learning: TensorFlow, Scikit-Learn, Pandas, PyTorch, Anomalias... </li>
+        <li>✔️ Governança e Gestão de TI: ITIL 4, COBIT 2019, BPMN, Arquitetura de Sistemas</li>
       </ul>
     </div>
   );
