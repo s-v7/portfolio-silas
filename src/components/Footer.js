f4eae64249f40db93de596a4c1273dceaf449c36
Silas Vasconcelos Cruz <svasconceloscruz7@gmail.com>
2025-02-15 22:44:51 -0300

    update:

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,20 @@
 import React from "react";
-import { FaFacebook, FaTwitter, FaInstagram, FaGithub } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="container mx-auto text-center">	  
-      <p>© 2025 Portofólio Silas Vasconcelos Cruz. Todos os direitos reservados.</p>
+      <p>© 2025 Silas Vasconcelos Cruz portfolio. All rights reserved.</p>
       <div className="social-icons">
-        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-          <FaFacebook />
-        </a>
-        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-          <FaTwitter />
-        </a>
-        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-          <FaInstagram />
-        </a>
-        <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://github.com/s-v7" target="_blank" rel="noopener noreferrer">
           <FaGithub />
+        </a>
+        <a href="https://www.linkedin.com/in/silas-v-053293255/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
+        <a href="mailto:svasconceloscruz7@gmail.com" target="_blank" rel="noopener noreferrer">
+          <FaEnvelope />
         </a>
       </div>
      </div>
--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -7,12 +7,12 @@ const Skills = () => {
       <h1 className="skills-title">Habilidades</h1>
       <ul className="skills-list">
         <li>✔️ Backend: Java(JDK(s) 8,11,17),(EJB, JPA, JSF, PrimeFaces), Python(Flask, Django), Node.js</li>
-        <li>✔️ Frontend: React, Angular, JavaScript</li>
-        <li>✔️ Banco de Dados: PostgreSQL, MySQL, SQLite, MongoDB</li>
-        <li>✔️ DevOps e Automação: Gitea, CI/CD, Webhooks, Docker, Kubernetes, Terraform, Ansible</li>
-        <li>✔️ Segurança: OWASP, Hardening de APIs, Pentest, Blockchain, Auditoria e Logs</li>
-        <li>✔️ IA e Machine Learning: TensorFlow, Scikit-Learn, Pandas, PyTorch, Anomalias... </li>
-        <li>✔️ Governança e Gestão de TI: ITIL 4, COBIT 2019, BPMN, Arquitetura de Sistemas</li>
+        <li>✔️ Frontend: React, Angular, JavaScript...</li>
+        <li>✔️ Database: PostgreSQL, MySQL, SQLite, MongoDB</li>
+        <li>✔️ DevOps and Automation: Gitea, CI/CD, Webhooks, Docker, Kubernetes, Terraform, Ansible</li>
+        <li>✔️ Security: OWASP, API Hardening, Pentest, Blockchain, Audit and Logs</li>
+        <li>✔️ AI and Machine Learning: TensorFlow, Scikit-Learn, Pandas, PyTorch, Anomalies... </li>
+        <li>✔️ <IT Governance and Management: ITIL 4, COBIT 2019, BPMN, Systems Architecture/li>
       </ul>
     </div>
   );
